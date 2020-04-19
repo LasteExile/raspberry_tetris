ba44745ff52b8b1679bea052223a9f52b7abfcb4
lasteexile <bbriz020903@gmail.com>
2020-04-19 13:49:10 +0300

    Edited coord system from the begining

--- main.py
+++ main.py
@@ -5,45 +5,42 @@ import random
 import render
 
 
-initial_y, initial_x = 0, 7
+initial_y, initial_x = 0, 0
 
 def main(game_field):
-    figure_index = random.randint(0, 3)
-    figure_rotation = random.randint(0, 3)
+    #figure_index = random.randint(0, 3)
+    #figure_rotation = random.randint(0, 3)
+    figure_index = 0
+    figure_rotation = 0
     y, x = initial_y, initial_x
+    figure = get_figure(figure_index, figure_rotation, y, x, (0, 0, 25))
+    game_field = update_game_field(game_field[:], figure)
+    render.show(game_field[:])
+    figure = get_figure(figure_index, figure_rotation, 7, x, (0, 0, 25))
+    game_field = update_game_field(game_field[:], figure)
+    render.show(game_field[:])
     while True:
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
                 if event.button == 1:
-                    pass
-        no_figure = get_figure(figure_index, figure_rotation, y, x, (0, 0, 0))
-        game_field = print_figure(no_figure, game_field)
-        figure = get_figure(figure_index, figure_rotation, y, x, (0, 0, 25))
-        y += 1
-        game_field = print_figure(figure, game_field)
-        render.show(game_field)
-        time.sleep(0.5)
-        
+                    x -= 1
 
-def print_figure(figure, game_field):
+def update_game_field(game_field, figure):
     for i in range(0, 5):
-        game_field[figure[0][i][0]][figure[0][i][1]] = figure[1][:]
-    return game_field[:]
+        game_field[figure[0][i][0]][figure[0][i][1]] = figure[1]
+    return game_field
 
 
 def get_figure(figure, rotation, y, x, color):
-    figures = [
-                [[[2, 0], [0, 1], [1, 1], [2, 1], [1, 2]], [[0, 0], [1, 0], [1, 1], [2, 1], [1, 2]], [[1, 0], [0, 1], [1, 1], [2, 1], [0, 2]], [[1, 0], [0, 1], [1, 1], [1, 2], [2, 2]]],
-                [[[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]], [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]], [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]], [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]]],
-                [[[0, 0], [0, 1], [1, 1], [2, 1], [3, 1]], [[1, 0], [1, 1], [1, 2], [1, 3], [0, 3]], [[0, 0], [1, 0], [2, 0], [3, 0], [0, 2]], [[0, 0], [0, 1], [0, 2], [0, 3], [1, 3]]],
-                [[[0, 0], [1, 0], [1, 1], [2, 1], [3, 1]], [[1, 0], [1, 1], [0, 2], [1, 2], [0, 3]], [[0, 0], [1, 0], [2, 0], [2, 1], [3, 1]], [[1, 0], [0, 1], [1, 1], [0, 2], [0, 3]]],
-            ]
+    figures = (
+            ([[1, 0], [2, 0], [0, 1], [1, 1], [1, 2]], [], [], []),
+            )
 
     needed_figure = [figures[figure][rotation], color]
     
     for i in range(0, 5):
+        needed_figure[0][i][1] += y 
         needed_figure[0][i][0] += x
-        needed_figure[0][i][1] += y
     
     return needed_figure[:]
 
@@ -63,14 +60,14 @@ def start_and_stop():
             [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], 
             [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], 
             [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], 
-            [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], 
+            [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)],
             [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], 
             [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]]
     while True:
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
                 if event.button == 10:
-                    main(game_field)
+                    main(game_field[:])
 
 def init():
     pygame.init()
